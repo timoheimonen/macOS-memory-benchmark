31fec64d3259fbed31c52eda6e7f6c47eeb6dc84
timoheimonen <timo.heimonen@gmail.com>
2026-04-05 13:39:49 +0300

    version 0.55.0 to development branch

--- src/core/config/version.h
+++ src/core/config/version.h
@@ -29,6 +29,6 @@
  * @def SOFTVERSION
  * @brief Software version number (semantic versioning format as string)
  */
-#define SOFTVERSION "0.55.0 EXPERIMENTAL"
+#define SOFTVERSION "0.55.0"
 
 #endif // VERSION_H
